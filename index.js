e219cbfc0487dcd6a4fc46b3d1133dcdb5a247bc
kim <kimemmanuel004@gmail.com>
2022-09-09 14:38:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Write your code here!
 const main = document.getElementById("main");
-main.remove('main');
+main.remove("main");
 
-const newHeader = document.createElement("h1");
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.textContent = 'YOUR-NAME is the champion';
 
 
 
